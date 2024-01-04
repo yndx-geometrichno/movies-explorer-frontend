f3b07ba543ae98703f980d84ee12c118ad568fd5
yndx_geometrichno <klementevart@yandex.ru>
2024-01-04 17:47:06 +0500

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
