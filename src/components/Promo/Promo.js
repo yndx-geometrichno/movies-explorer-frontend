de94764af371d38287e156b5a8bcd016d2fe9997
yndx_geometrichno <klementevart@yandex.ru>
2024-03-12 23:54:27 +0500

    feat: promo banner for main page

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Promo.css";
+
+function Promo() {
+  return (
+    <section className="content__lead">
+      <h1 className="content__lead_header">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+    </section>
+  );
+}
+
+export default Promo;
