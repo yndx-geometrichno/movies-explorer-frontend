15164f284363d0688fdb1803f0088309b6365747
yndx_geometrichno <klementevart@yandex.ru>
2024-03-12 23:54:59 +0500

    feat: register component for user registration

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useState } from "react";
+import SignForm from "../SignForm/SignForm";
+import { Link } from "react-router-dom";
 
-function Register() {
+function Register({ onRegister }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onRegister(password, email, name);
+  }
+
   return (
-    <div>Register</div>
-  )
+    <SignForm
+      header="Добро пожаловать!"
+      email={email}
+      name={name}
+      password={password}
+      onSubmit={handleSubmit}
+      setName={setName}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      buttonText="Зарегистрироваться"
+      formType="form_type_register"
+    >
+      <p className="sign__link-container">
+        Уже зарегистрированы?
+        <Link to="/signin" className="sign__link">
+          Войти
+        </Link>
+      </p>
+    </SignForm>
+  );
 }
 
-export default Register
+export default Register;
