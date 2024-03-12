8d057c6a157ae72578b5e1224f84b528f6b3df93
yndx_geometrichno <klementevart@yandex.ru>
2024-03-12 23:50:42 +0500

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import SignForm from "../SignForm/SignForm";
+import { Link } from "react-router-dom";
 
-function Login() {
+function Login({ onLogin }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onLogin(password, email);
+  }
+
   return (
-    <div>Login</div>
-  )
+    <SignForm
+      header="Рады видеть!"
+      email={email}
+      password={password}
+      onSubmit={handleSubmit}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      buttonText="Войти"
+      formType="form_type_login"
+    >
+      <p className="sign__link-container">
+        Еще не зарегистрированы?
+        <Link to="/signup" className="sign__link">
+          Регистрация
+        </Link>
+      </p>
+    </SignForm>
+  );
 }
 
-export default Login
+export default Login;
